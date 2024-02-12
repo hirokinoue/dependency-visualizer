92e8019f6bfce02bcafeab6f171388a95b45d5e0
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2024-02-12 22:57:37 +0900

    refactor: 詳細への依存を減らしリファクタへの耐性を高める

--- tests/ClassVisitorTest.php
+++ tests/ClassVisitorTest.php
@@ -4,6 +4,7 @@ namespace Hirokinoue\DependencyVisualizer\Tests;
 
 use Hirokinoue\DependencyVisualizer\ClassVisitor;
 use Hirokinoue\DependencyVisualizer\DiagramUnit;
+use Hirokinoue\DependencyVisualizer\StringExporter;
 use PhpParser\Node\Stmt;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitor\NameResolver;
@@ -19,27 +20,31 @@ final class ClassVisitorTest extends TestCase
     {
         // given
         /** @var string $code */
-        $code = file_get_contents(__DIR__ . '/data/RootClass.php');
+        $code = file_get_contents(__DIR__ . '/data/root.php');
         $parser = (new ParserFactory())->createForHostVersion();
         /** @var Stmt[] $stmts */
         $stmts = $parser->parse($code);
+        $sut = new ClassVisitor($diagramUnit = new DiagramUnit('root', ['root']));
         $nodeTraverser = new NodeTraverser();
         $nodeTraverser->addVisitor(new NameResolver());
-        $namespace = '\Hirokinoue\DependencyVisualizer\Tests\data';
-        $nodeTraverser->addVisitor(new ClassVisitor($diagramUnit = new DiagramUnit($namespace . '\RootClass', [$namespace . '\RootClass'])));
+        $nodeTraverser->addVisitor($sut);
 
-        $expected = new DiagramUnit($namespace . '\RootClass', [$namespace . '\RootClass']);
-        $baz = new DiagramUnit($namespace . '\Baz', [$namespace . '\RootClass', $namespace . '\Baz']);
-        $baz->push(new DiagramUnit($namespace . '\Qux', [$namespace . '\RootClass', $namespace . '\Baz', $namespace . '\Qux']));
-        $expected->push(new DiagramUnit('\Error', [$namespace . '\RootClass', '\Error']));
-        $expected->push(new DiagramUnit($namespace . '\Bar', [$namespace . '\RootClass', $namespace . '\Bar']));
-        $expected->push($baz);
-        $expected->push(new DiagramUnit('\stdClass', [$namespace . '\RootClass', '\stdClass']));
+        $expected = <<<RESULT
+root
+  \Hirokinoue\DependencyVisualizer\Tests\data\Bar
+  \Hirokinoue\DependencyVisualizer\Tests\data\Baz
+    \Hirokinoue\DependencyVisualizer\Tests\data\Qux
+  \stdClass
+
+RESULT;
 
         // when
         $nodeTraverser->traverse($stmts);
 
+        $stringExporter = new StringExporter();
+        $result = $stringExporter->export($diagramUnit);
+
         // then
-        $this->assertEquals($expected, $diagramUnit, print_r($diagramUnit, true));
+        $this->assertEquals($expected, $result);
     }
 }
