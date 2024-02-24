140a6c0897d898cddac778cad88e91c4c28dc63f
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2024-02-24 14:06:41 +0900

    test: 設定を読み込むテストケースを追加する

---
+++ tests/Config/ConfigTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Hirokinoue\DependencyVisualizer\Tests\Config;
+
+use Hirokinoue\DependencyVisualizer\Config\Config;
+use PHPUnit\Framework\TestCase;
+
+final class ConfigTest extends TestCase
+{
+    /**
+     * @noinspection NonAsciiCharacters
+     */
+    public function test指定したファイルから設定を取得できること(): void
+    {
+        // given
+        Config::initialize(__DIR__ . '/../data/Config');
+
+        // when
+        $memoryLimit = Config::memoryLimit();
+        $endOfAnalysis = Config::endOfAnalysis();
+
+        // then
+        $this->assertSame('Foo', $memoryLimit);
+        $this->assertSame(['Bar'], $endOfAnalysis);
+    }
+}
