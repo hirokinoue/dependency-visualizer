f851cf44065cea4c0a372b7e5df51876c1b51141
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2024-02-24 13:52:29 +0900

    feat: 設定ファイルを利用できるようにする

---
+++ src/Config/Config.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Hirokinoue\DependencyVisualizer\Config;
+
+class Config
+{
+    private static string $memoryLimit = '1024M';
+    /** @var string[] */
+    private static array $endOfAnalysis = [];
+
+    public static function initialize(string $baseDir): void
+    {
+        $config = [];
+        foreach (['config.php', 'config.php.dist', 'config.dist.php'] as $discoverableConfigName) {
+            $discoverableConfigFile = $baseDir . DIRECTORY_SEPARATOR . $discoverableConfigName;
+            if (\is_file($discoverableConfigFile)) {
+                $config = require $discoverableConfigFile;
+                if (!\is_array($config)) {
+                    throw new \InvalidArgumentException('Config file format is invalid: ' . $discoverableConfigFile);
+                }
+            }
+        }
+        self::$memoryLimit = $config['memoryLimit'] ?? '1024M';
+        self::$endOfAnalysis = $config['endOfAnalysis'] ?? [];
+    }
+
+    public static function memoryLimit(): string
+    {
+        return self::$memoryLimit;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function endOfAnalysis(): array
+    {
+        return self::$endOfAnalysis;
+    }
+}
